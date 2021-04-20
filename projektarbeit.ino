54b9f5fc931173797c5d8c5feba1527f575ba431
rAiZeNXD <72040243+rAiZeNXD@users.noreply.github.com>
2021-04-20 16:29:52 +0200

    new features, better organizing and fixes

--- projektarbeit.ino
+++ projektarbeit.ino
@@ -3,97 +3,34 @@
 #include <NTPClient.h>
 #include <ArduinoMqttClient.h>
 
+// Globale Variablen
+
+char ssid[30], passwd[30], broker[30], dataSent[30];
+int pinCount, port, countTopic, i = 0, j, valves[40], valvesPin[40];
+bool establishedVALVES = false, establishedWIFI = false, establishedMQTT = false;
+enum Betriebmodus { halt, manuell, automatisch};
+String dataRecieved, topics[41];
+long timeNow, timePrev;
+
+// Objekte
+
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
-
-
-int        port; //1883;
-const char topic[]  = "sex/simple";
-
-
-#define V1 23
-#define V2 22
-#define V3 21
-#define V4 19
-
-char ssid[30];
-char passwd[30];
-char broker[30]; //"test.mosquitto.org"
-char brokerPort[6];
-char dataSent[30];
-int valveCount;
-int i = 0;
-bool gotSSID = false, gotPASSWD = false, gotBROKER = false, gotBROKERport = false;
-
-enum Betriebmodus { halt, manuell, automatisch};
 Betriebmodus modus;
-
-enum Zustaende { offen, geschlossen };
-Zustaende stateV1, stateV2, stateV3, stateV4;
-
-
-String dataRecieved;
-long timeNow, timePrev;
 
 void setup() 
 {
   Serial.begin(115200);
-  delay(250);
+  delay(1000);
   Serial.println("ESP32 started");
   delay(500);
-  
+  createPins();
   establishConnectionWiFi();
-  
-  while (!gotBROKER && !gotBROKERport)
-  {
-    if (Serial.available())
-    {
-      while (Serial.available())
-      {
-       broker[i++] = Serial.read();
-       delay(100); 
-      } 
-      broker[i-1] = '\0';
-      i = 0;
-      Serial.println(broker);
-      gotBROKER = true;
-      Serial.println("\nPort:");
-
-      while (!Serial.available())
-        delay(100);
-        
-      while (Serial.available())
-      {
-       brokerPort[i++] = Serial.read();
-       delay(100); 
-      }
-
-      brokerPort[i-1] = '\0';
-      port = (int)brokerPort;
-      i = 0;
-      gotBROKERport = true;
-    }
-  }
-  
-  timeClient.begin();
-   if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-
-    while (1);
-  }
-
-  Serial.println("\nYou're connected to the MQTT broker!");
-  Serial.println();
-
-  pinMode(V1, OUTPUT);
-  pinMode(V2, OUTPUT);
-  pinMode(V3, OUTPUT);
-  pinMode(V4, OUTPUT);
+  establishConnectionMQTT();
   modus = halt;
-  Serial.println("\nSetup Completed!\n To see all commands type: help");
+  Serial.println("Setup Completed!\n To see all commands type: help");
 }
 
 void loop() 
@@ -108,8 +45,163 @@ void loop()
   delay(500);
 }
 
-/* Diese Funktion überprüft ob etwas über die Serielle Schnitstelle gesendet wurde
-#  Wenn ja, dann speichert es die einzele Bits in CharArray und dann umwandelt CharArray in String um */ 
+// Funktionen
+
+void createPins()
+{
+  Serial.println("How many Valves?:");
+
+  while (!establishedVALVES)
+  {
+    if (Serial.available())
+    {
+      while (Serial.available())
+      {
+        dataSent[i++] = Serial.read();
+        delay(100);
+      }
+      dataSent[i-1] = '\0'; //Serial in Arduino
+      pinCount = atoi(dataSent);
+      Serial.println(pinCount);
+      i = 0;
+      establishedVALVES = true;
+
+      for (j = 1; j <= pinCount; j++)
+      {
+        Serial.printf("Assign Pin (On Board) to current Pin (%2d)\n", j);
+        
+        while (!Serial.available())
+          delay(250);
+
+        while (Serial.available())
+        {
+          dataSent[i++] = Serial.read();
+          delay(100);
+        }
+        dataSent[i-1] = '\0'; //Serial in Arduino
+        valves[j] = atoi(dataSent);
+        pinMode(valves[j], OUTPUT);
+        i = 0;
+        Serial.println("Assign topic to current Pin:");
+
+        while (!Serial.available())
+          delay(250);
+
+        while (Serial.available())
+        {
+          dataSent[i++] = Serial.read();
+          delay(100);
+        }
+        dataSent[i-1] = '\0'; //Serial in Arduino
+        topics[j] = dataSent;
+        i = 0;
+        Serial.println(topics[j]);
+      }
+      topics[j] = "projektarbeit/main";
+      Serial.println(topics[5]);
+    }
+  }  
+}
+
+void establishConnectionWiFi()
+{
+  Serial.println("SSID of your Network:");
+  while (!establishedWIFI)
+  {
+    delay(100);
+    if (Serial.available())
+    {
+      while (Serial.available())
+      {
+       ssid[i++] = Serial.read();
+       delay(100); 
+      } 
+  
+      ssid[i-1] = '\0';
+      i = 0;
+      Serial.println("Password:");
+      
+      while (!Serial.available())
+        delay(100);
+        
+      while (Serial.available())
+      {
+       passwd[i++] = Serial.read();
+       delay(100); 
+      }
+
+      passwd[i-1] = '\0';
+      i = 0;
+      establishedWIFI = true;
+    }
+  }
+  delay(200);
+  WiFi.begin(ssid, passwd);
+  Serial.printf("\nConnecting to %s", ssid);
+
+  while (WiFi.status() != WL_CONNECTED)
+    delay(500);
+
+  Serial.println("\nConnection succeed!");
+  Serial.print("Local IP: ");
+  Serial.print(WiFi.localIP());
+}
+
+void establishConnectionMQTT()
+{
+  Serial.println("\nIP Address or Domain of MQTT Server:");
+  while (!establishedMQTT)
+  {
+    delay(100);
+    if (Serial.available())
+    {
+      while (Serial.available())
+      {
+       broker[i++] = Serial.read();
+       delay(100); 
+      } 
+      broker[i-1] = '\0';
+      i = 0;
+      Serial.println("Port:");
+
+      while (!Serial.available())
+        delay(100);
+        
+      while (Serial.available())
+      {
+        dataSent[i++] = Serial.read();
+        delay(100);
+      }
+      dataSent[i-1] = '\0'; //Serial in Arduino
+      i = 0;
+      port = atoi(dataSent);
+      establishedMQTT = true;
+    }
+  }
+  
+  timeClient.begin();
+   if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("\nYou're connected to the MQTT broker!");
+}
+
+void checkMQTT()
+{
+  if (mqttClient.available())
+  {
+    while (mqttClient.available())
+      dataRecieved[i++] = mqttClient.read();
+  }
+}
+
+// Diese Funktion überprüft ob etwas über die Serielle Schnitstelle gesendet wurde.
+// Wenn ja, dann speichert es die einzele Bits in CharArray und dann umwandelt CharArray in String um.
+
 void serialCheckAndSetMode()
 {
   while (Serial.available() > 0 && Serial.available() < 30)
@@ -131,37 +223,37 @@ void serialCheckAndSetMode()
   else if (dataRecieved == "showip")
   {
    Serial.println(WiFi.localIP());
-   mqttClient.beginMessage(topic);
-   mqttClient.print("hello ");
+   mqttClient.beginMessage(topics[j]);
+   Serial.println(topics[j]);
+   mqttClient.print("hello");
    mqttClient.endMessage();
   }
   else if (dataRecieved == "time")
     Serial.println("Uhr: " + timeClient.getFormattedTime());
-  else if (dataRecieved == "connect")
-    establishConnectionWiFi();  
+  else if (dataRecieved == "printalltopics")
+    printAllTopics();  
   else if (dataRecieved == "help")
   {
-    //
-    Serial.println("Commands: \nhelp - Shows this list of commands");
+    Serial.println("Commands:");
     Serial.println("stop - Disconnect from Network and close all Valves");
     Serial.println("manual - Switches to Manual Mode. In this Mode User can open/close specific Valve");
     Serial.println("automatic - Switches to Automatic Mode. In this Mode all Valves are being opened and closed frequently");
     Serial.println("showip - Shows current local IP");
     Serial.println("time - Shows current Time in 24H unit"); 
-    Serial.println("connect - Opens ");
   }
     
   // Befehle für Debug Steuerung
   if (modus == manuell)
   {
+    /*
     if (dataRecieved == "1 open")
-      stateV1 = offen;
+      //stateV1 = offen;
     else if (dataRecieved == "1 close")
-      stateV1 = geschlossen;
+      //stateV1 = geschlossen;
     else if (dataRecieved == "2 open")
-      stateV2 = offen;
+      //stateV2 = offen;
     else if (dataRecieved == "2 close")
-      stateV2 = geschlossen;
+      //stateV2 = geschlossen;
     else if (dataRecieved == "3 open")
       stateV3 = offen;
     else if (dataRecieved == "3 close")
@@ -170,10 +262,8 @@ void serialCheckAndSetMode()
       stateV4 = offen;
     else if (dataRecieved == "4 close") 
       stateV4 = geschlossen;
+      */
   }
-
-  // Befehle für automatische Steuerung
-  
   i = 0;
   dataRecieved = "";
 }
@@ -183,7 +273,7 @@ void steuerung()
   switch (modus)
   {
     case manuell: // Manuelle Steuerung
-
+      /*
       if (stateV1 == offen) 
       { 
         digitalWrite(V1, HIGH); 
@@ -213,7 +303,7 @@ void steuerung()
         digitalWrite(V4, HIGH);
       else if (stateV4 == geschlossen)
         digitalWrite(V4, LOW);
-        
+        */
     break;
 
     case automatisch: // Automatische Steuerung
@@ -237,62 +327,27 @@ void steuerung()
   }
 }
 
-void establishConnectionWiFi()
-{
-  Serial.println("SSID of your Network:");
-  while (!gotSSID && !gotPASSWD)
-  {
-    if (Serial.available())
-    {
-      while (Serial.available())
-      {
-       ssid[i++] = Serial.read();
-       delay(100); 
-      } 
-  
-      ssid[i-1] = '\0';
-      i = 0;
-      gotSSID = true;
-      Serial.println("Passwort:");
-      
-      while (!Serial.available())
-        delay(100);
-        
-      while (Serial.available())
-      {
-       passwd[i++] = Serial.read();
-       delay(100); 
-      }
-
-      passwd[i-1] = '\0';
-      i = 0;
-      gotPASSWD = true;
-    }
-  }
-  delay(200);
-  WiFi.begin(ssid, passwd);
-  Serial.printf("\nConnecting to %s", ssid);
-
-  while (WiFi.status() != WL_CONNECTED)
-    delay(500);
-
-  Serial.println("\nConnection succeed!");
-  Serial.print("Local IP: ");
-  Serial.print(WiFi.localIP());
-}
-
 void openAllValves()
 {
-  digitalWrite(V1, HIGH);
-  digitalWrite(V2, HIGH);
-  digitalWrite(V3, HIGH);
-  digitalWrite(V4, HIGH);
+  for (j = 1; j <= pinCount; j++)
+  {
+    digitalWrite(valves[j], HIGH);
+  }
 }
 
 void closeAllValves()
 {
-  digitalWrite(V1, LOW);
-  digitalWrite(V2, LOW);
-  digitalWrite(V3, LOW);
-  digitalWrite(V4, LOW);
+  for (j = 1; j <= pinCount; j++)
+  {
+    digitalWrite(valves[j], LOW);
+  }
+}
+
+void printAllTopics()
+{
+  for (j = 1; j <= pinCount; j++)
+  {
+    Serial.println(topics[j]);
+  }
+  Serial.println(topics[j]);
 }
